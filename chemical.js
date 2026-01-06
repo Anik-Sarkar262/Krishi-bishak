ae5153ce0d52ee5f5f0d44783a142fedf8975826
naskarayan1816-eng <naskarayan1816@gmail.com>
2026-01-06 13:24:17 +0530

    Update chemical.js

--- chemical.js
+++ chemical.js
@@ -1,4 +1,4 @@
-const API_KEY = "AIzaSyBirCT-JxMP89PQKwp13d5T60UI-PbZoyo"; 
+const API_KEY = "AIzaSyAcYpU8kYrtQGaQK5IWJSb1iFOI5hpQ3Z0"; 
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 
 // SELECTORS (Make sure these IDs match your HTML file!)
